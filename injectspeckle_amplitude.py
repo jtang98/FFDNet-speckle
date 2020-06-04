5ca4fa7afec36c27f7a830c8cb0b38488d36f9ec
Joël Tang <44188317+jtang98@users.noreply.github.com>
2020-06-04 16:31:07 +0200

    Sets of speckled images L=1,3 and new pipeline for speckle noising

--- injectspeckle_amplitude.py
+++ injectspeckle_amplitude.py
@@ -7,6 +7,8 @@ Created on Wed Apr  4 13:12:23 2018
 """
 import numpy as np
 import skimage.io
+from os import listdir
+from os.path import isfile, join
 
 def injectspeckle_amplitude(img,L):
     rows = img.shape[0]
@@ -19,14 +21,12 @@ def injectspeckle_amplitude(img,L):
     ima_speckle_amplitude = np.multiply(img,s_amplitude)
     return ima_speckle_amplitude
 
-im = skimage.io.imread('testsets/set5/butterfly.bmp')
-im = 0.2125 * im[:,:,0] + 0.7154 * im[:,:,1] + 0.0721 * im[:,:,2]
-speckled_image = injectspeckle_amplitude(im, 3)
-speckled_image = speckled_image.astype('uint8')
-skimage.io.imsave('testsets/set_speckle/butterfly.bmp',speckled_image)
 
+filenames = [f for f in listdir('testsets/set5/') if isfile(join('testsets/set5/', f))]
 
-'''im = skimage.io.imread('testsets/set_speckle/baby.bmp')
-im = np.log(im + 10**(-15))
-im_ = im[0:50, 0:50]**2
-print(np.sqrt(np.var(np.ravel(im_), dtype=np.float64)))'''
+for filename in filenames:
+    im = skimage.io.imread('testsets/set5/' + filename)
+    im = 0.2125 * im[:,:,0] + 0.7154 * im[:,:,1] + 0.0721 * im[:,:,2]
+    speckled_image = injectspeckle_amplitude(im, 3)
+    speckled_image = speckled_image.astype('uint8')
+    skimage.io.imsave('testsets/set_speckle_L3/' + filename, speckled_image)
